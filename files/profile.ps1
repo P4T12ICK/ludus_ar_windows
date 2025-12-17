8eec8e6319e54d01f83527403386e2c9836d6289
P4T12ICK <pbareiss@splunk.com>
2025-12-17 07:53:52 +0100

    improvements to profile.ps1

--- files/profile.ps1
+++ files/profile.ps1
@@ -1,4 +1,12 @@
 
-Import-Module powershell-yaml
-Import-Module "C:\AtomicRedTeam\invoke-atomicredteam\Invoke-AtomicRedTeam.psd1" -Force 
-$PSDefaultParameterValues = @{"Invoke-AtomicTest:PathToAtomicsFolder"="C:\AtomicRedTeam\atomics"}
+# Import powershell-yaml if available
+if (Get-Module -ListAvailable -Name powershell-yaml) {
+    Import-Module powershell-yaml -ErrorAction SilentlyContinue
+}
+
+# Import Invoke-AtomicRedTeam if available
+$atomicRedTeamModule = Get-Module -ListAvailable -Name invoke-atomicredteam
+if ($atomicRedTeamModule) {
+    Import-Module invoke-atomicredteam -Force -ErrorAction SilentlyContinue
+    $PSDefaultParameterValues = @{"Invoke-AtomicTest:PathToAtomicsFolder"="C:\AtomicRedTeam\atomics"}
+}
